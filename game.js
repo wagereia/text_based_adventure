209b3d91e1a4fc6ab96b5c8451829ec1fb230d12
anjingliaran <kidefuzambranam3115185nhgy@hotmail.com>
2024-10-12 00:48:11 +0000

    adding game.js

--- game.js
+++ game.js
@@ -1,77 +1,59 @@
-const readline = require('readline');
-const story = require('./story.json');
-const gameState = require('./gameState.js');
+import readline from 'readline';
+import { gameData } from './gameData.js';
 
 class AdventureGame {
-    constructor() {
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        this.currentSceneId = 'start';
-    }
+  constructor() {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    this.currentScene = 'start';
+  }
 
-    // Display current scene and choices
-    displayScene() {
-        const scene = story.scenes[this.currentSceneId];
-        
-        console.log('\n' + '='.repeat(50));
-        console.log(scene.text);
-        console.log('='.repeat(50));
-        
-        if (scene.choices && scene.choices.length > 0) {
-            console.log('\nYour choices:');
-            scene.choices.forEach((choice, index) => {
-                console.log(`${index + 1}. ${choice.text}`);
-            });
-            this.promptChoice();
-        } else {
-            // End of game
-            console.log('\n--- THE END ---');
-            gameState.saveGame(this.currentSceneId);
-            this.rl.close();
-        }
+  // Display current scene and prompt for choice
+  displayScene() {
+    const scene = gameData.scenes[this.currentScene];
+    
+    console.log('\n' + '='.repeat(50));
+    console.log(scene.text);
+    console.log('='.repeat(50));
+    
+    if (scene.choices && scene.choices.length > 0) {
+      console.log('\nYour choices:');
+      scene.choices.forEach((choice, index) => {
+        console.log(`${index + 1}. ${choice.text}`);
+      });
+      this.promptChoice(scene.choices);
+    } else {
+      // End of game
+      console.log('\nThanks for playing!');
+      this.rl.close();
     }
+  }
 
-    // Prompt user for choice
-    promptChoice() {
-        const scene = story.scenes[this.currentSceneId];
-        
-        this.rl.question('\nEnter your choice (1-' + scene.choices.length + '): ', (answer) => {
-            const choiceIndex = parseInt(answer) - 1;
-            
-            if (choiceIndex >= 0 && choiceIndex < scene.choices.length) {
-                const selectedChoice = scene.choices[choiceIndex];
-                this.currentSceneId = selectedChoice.nextScene;
-                gameState.saveGame(this.currentSceneId);
-                this.displayScene();
-            } else {
-                console.log('Invalid choice! Please try again.');
-                this.promptChoice();
-            }
-        });
-    }
-
-    // Start the game
-    start() {
-        console.log('🎮 Welcome to the Forest Adventure! 🎮');
-        console.log('Make choices by entering numbers. Good luck!\n');
-        
-        // Try to load saved game
-        const savedScene = gameState.loadGame();
-        if (savedScene && story.scenes[savedScene]) {
-            this.currentSceneId = savedScene;
-            console.log('Loaded saved game...\n');
-        }
-        
+  // Handle user input for choices
+  promptChoice(choices) {
+    this.rl.question('\nEnter your choice (1-' + choices.length + '): ', (answer) => {
+      const choiceIndex = parseInt(answer) - 1;
+      
+      if (choiceIndex >= 0 && choiceIndex < choices.length) {
+        this.currentScene = choices[choiceIndex].nextScene;
         this.displayScene();
-    }
+      } else {
+        console.log('Invalid choice! Please try again.');
+        this.promptChoice(choices);
+      }
+    });
+  }
+
+  // Start the game
+  start() {
+    console.log('Welcome to the Adventure Game!');
+    console.log('Make choices by entering numbers. Good luck!\n');
+    this.displayScene();
+  }
 }
 
-// Start the game if this file is run directly
-if (require.main === module) {
-    const game = new AdventureGame();
-    game.start();
-}
-
-module.exports = AdventureGame;
+// Start the game
+const game = new AdventureGame();
+game.start();
